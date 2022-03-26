c2fe23454e401870865fa5ee8171fbb9a5162772
unknown <gamemudev010@gmail.com>
2022-03-26 13:02:04 +0100

    Initial Commit

--- main.py
+++ main.py
@@ -2,3 +2,16 @@
 
 # VARIABLES
 text = input("Enter text: ").lower()
+
+# FUNCTIONS
+
+
+def convert_to_morse(text):
+    result = ""
+
+    return result
+
+
+# END-USER
+
+print(f"Encrypted Text: {convert_to_morse(text)}")
